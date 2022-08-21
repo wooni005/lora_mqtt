d39c337509f1fb694f2b83f848bff01d9eb26454
wooni005 <arjan@wooning.cz>
2022-08-21 21:20:14 +0200

    Lot of changes: weatherstation, detection for DS18b20 device disconnected, commented out wilddetector and nodeId changes for temp devices

--- lora_mqtt.py
+++ lora_mqtt.py
@@ -18,6 +18,8 @@ import logger
 import serviceReport
 import settings
 
+DEVICE_DISCONNECTED = -127
+
 wildDetectorActive = True
 chickenDoorOpenActive = False
 
@@ -27,7 +29,6 @@ current_sec_time = lambda: int(round(time.time()))
 exit = False
 serialPort = None
 LoRaRSSI = False
-
 
 def signal_handler(_signal, frame):
     global exit
@@ -196,50 +197,170 @@ def serialPortThread():
                         else:
                             print("LoRa: Received unknown msgId:%d from NodeId %d" % (msgId, nodeId))
 
-                    # nodeId 3: Wild detector
-                    elif nodeId == 3:
-                        msgId = int(msg[0])
+                    # # nodeId 3: Wild detector
+                    # elif nodeId == 3:
+                    #     msgId = int(msg[0])
 
-                        # MSG_ID_NODE_STARTUP=1: Node startup notification
-                        if msgId == 1:
-                            # This sensor is still available
-                            mqtt_publish.single("huis/LoRa/PIRB-Wild-Detector/pirb", 0, qos=1, hostname=settings.MQTT_ServerIP)
-                        # MSG_ID_STILL_ALIVE=2: Node still alive
-                        elif msgId == 2:
-                            # This sensor is still available
-                            mqtt_publish.single("huis/LoRa/PIRB-Wild-Detector/pirb", 0, qos=1, hostname=settings.MQTT_ServerIP)
-                        # MSG_ID_CMND_REQUEST=3: Node wakeup/cmnd request
-                        elif msgId == 3:
-                            if wildDetectorActive:
-                                # Set WildDetector PIR: off
-                                sendQueue.put("3,1,%ds".encode() % nodeId)
-                                print("LoRa: Activate WildDetector PIR")
-                            else:
-                                # Set WildDetector PIR: off
-                                sendQueue.put("3,0,%ds".encode() % nodeId)
-                                print("LoRa: De-activate WildDetector PIR")
-                        # MSG_ID_PIR_MOVEMENT=4: PIR detected movement
-                        elif msgId == 4:
-                            mqtt_publish.single("huis/LoRa/PIRB-Wild-Detector/pirb", 1, qos=1, hostname=settings.MQTT_ServerIP)
-                        else:
-                            print("LoRa: Received unknown msgId:%d from NodeId %d" % (msgId, nodeId))
+                    #     # MSG_ID_NODE_STARTUP=1: Node startup notification
+                    #     if msgId == 1:
+                    #         # This sensor is still available
+                    #         mqtt_publish.single("huis/LoRa/PIRB-Wild-Detector/pirb", 0, qos=1, hostname=settings.MQTT_ServerIP)
+                    #     # MSG_ID_STILL_ALIVE=2: Node still alive
+                    #     elif msgId == 2:
+                    #         # This sensor is still available
+                    #         mqtt_publish.single("huis/LoRa/PIRB-Wild-Detector/pirb", 0, qos=1, hostname=settings.MQTT_ServerIP)
+                    #     # MSG_ID_CMND_REQUEST=3: Node wakeup/cmnd request
+                    #     elif msgId == 3:
+                    #         if wildDetectorActive:
+                    #             # Set WildDetector PIR: off
+                    #             sendQueue.put("3,1,%ds".encode() % nodeId)
+                    #             print("LoRa: Activate WildDetector PIR")
+                    #         else:
+                    #             # Set WildDetector PIR: off
+                    #             sendQueue.put("3,0,%ds".encode() % nodeId)
+                    #             print("LoRa: De-activate WildDetector PIR")
+                    #     # MSG_ID_PIR_MOVEMENT=4: PIR detected movement
+                    #     elif msgId == 4:
+                    #         mqtt_publish.single("huis/LoRa/PIRB-Wild-Detector/pirb", 1, qos=1, hostname=settings.MQTT_ServerIP)
+                    #     else:
+                    #         print("LoRa: Received unknown msgId:%d from NodeId %d" % (msgId, nodeId))
 
                     # nodeId 4: Weerstation
                     elif nodeId == 4:
-                        pass
+                        # byte nodeId;
+                        # byte msgId;
+                        # byte voltageVcc;				 //getVcc 1.0V=0, 1.8V=40, 3,0V=100, 3.3V=115, 5.0V=200, 6.0V=250
+                        # int temperature;
+                        # int humidity;
+                        # int pressure;
+                        # byte windDirection;
+                        # unsigned int windPulses;		//Total nr of wind pulses in 5 min: 39294 pulses by 160km/h
+                        # unsigned int windGust; 			//Max nr of measured pulses in a 8 sec frame
+                        # unsigned int rainPulses; 		//Total nr of rain pulses in 5 min
 
-                    # nodeId 5: Temp Schuur
-                    # nodeId 6: Temp Chata
-                    # nodeId 7: Temp Vriezer Werkplaats
-                    # nodeId 8: Temp Vriezer Keuken
-                    # nodeId 9: Temp Vriezer Technische ruimte
-                    elif (nodeId >= 5) and (nodeId <= 9):
                         msgId = int(msg[0])
                         del msg[0]  # remove msgId from list
 
                         # MSG_ID_NODE_STARTUP=1: Node startup notification
                         if msgId == 1:
-                            pass
+                            print("Received Weather station node startup notification")
+                        # MSG_ID_TEMPERATURE=4: Temperature measurement
+                        elif msgId == 4:
+                            sensorData = {}
+
+                            #Vcc: msg[0] -> 1.0V=0, 1.8V=40, 3,0V=100, 3.3V=115, 5.0V=200, 6.0V=250
+                            voltageVcc = float(((int(msg[0]) * 2) + 100)) / 100
+                            sensorData['Vcc'] = round(voltageVcc, 2)
+
+                            # Temp (int)
+                            byteVal = bytearray([int(msg[2]), int(msg[1])])
+                            val = struct.unpack(">h", byteVal)[0]
+                            sensorData['Temperature'] = round(float(val) / 100, 1)
+                            print("Temp Buiten %1.1f C" % (val / 100))
+
+                            # Humidity (int)
+                            byteVal = bytearray([int(msg[4]), int(msg[3])])
+                            val = struct.unpack(">h", byteVal)[0]
+                            sensorData['Humidity'] = round(float(val) / 100, 1)
+
+                            mqtt_publish.single("huis/LoRa/Weerstation/temp", json.dumps(sensorData, separators=(', ', ':')), hostname=settings.MQTT_ServerIP, retain=True)
+
+                            # Pressure (int)
+                            byteVal = bytearray([int(msg[6]), int(msg[5])])
+                            val = struct.unpack(">h", byteVal)[0]
+                            sensorData['Pressure'] = round(float(val) / 10, 1)
+
+                            altituteInMeter = 500  # 500 m
+                            pressureSealevel = val / pow((1.0 - float(altituteInMeter) / float(44330)), 5.255)
+                            sensorData['PressureSealevel'] = round(pressureSealevel / 10, 1)
+                        
+                            # 8 bits=360 degrees
+                            windContactToDegrees = {
+                                0x01: 0,        # 0000 0001 - 0x01 -   1 - 0°    (North)
+                                0x03: 22.5,     # 0000 0011 - 0x03 -   3 - 22,5°
+                                0x02: 45,       # 0000 0010 - 0x02 -   2 - 45°   (North-East)
+                                0x06: 67.5,     # 0000 0110 - 0x06 -   6 - 67,5°
+                                0x04: 90,       # 0000 0100 - 0x04 -   4 - 90°   (East)
+                                0x0C: 112.5,    # 0000 1100 - 0x0C -  12 - 112,5°
+                                0x08: 135,      # 0000 1000 - 0x08 -   8 - 135°  (South-East)
+                                0x18: 157.5,    # 0001 1000 - 0x18 -  24 - 157,5°
+                                0x10: 180,      # 0001 0000 - 0x10 -  16 - 180°  (South)
+                                0x30: 202.5,    # 0011 0000 - 0x30 -  48 - 202,5°
+                                0x20: 225,      # 0010 0000 - 0x20 -  32 - 225°  (South-West)
+                                0x10: 247.5,    # 0110 0000 - 0x10 -  96 - 247,5°
+                                0x40: 270,      # 0100 0000 - 0x40 -  64 - 270°  (West)
+                                0x60: 292.5,    # 1100 0000 - 0x60 - 192 - 292,5°
+                                0x80: 315,      # 1000 0000 - 0x80 - 128 - 315°  (North-West)
+                                0x81: 337.5     # 1000 0001 - 0x81 - 129 - 337,5°
+                            }
+                            try:
+                                # Wind direction (byte)
+                                sensorData['WindDirection'] = windContactToDegrees[int(msg[7])]
+                            except:
+                                sensorData['WindDirection'] = 0
+                                print("Value: 0x%02x not found in windContactToDegrees table" % (int(msg[7])))
+                            # sensorData['WindDirection'] = int(msg[7])
+
+                            # Windspeed in past 5 min (unsigned int)
+                            # 1 rotation of wind of the anemometer (circumference) is 678.6mm = 0.6786m and gives 2 pulses
+                            # Time measurement frame of the wind pulses is 5 min = 300 sec
+                            # Rotations = windPulses / 2
+                            # distance = 0.6786m * rotations
+                            # windspeed = distance / 300
+                            #
+                            # windSpeed [m/s] = windPulses / 2 * 0.6786 / 300 = windPulses * 0.001131
+                            # anemometer correction factor = 1.18 -> 0.001131 * 1.18 = 0.00133458
+                            # Conversion m/s to km/h = m/s * 3.6
+                            # windSpeed [km/h] = 0.00133458 * 3.6 = 0.004804488
+                            byteVal = bytearray([int(msg[9]), int(msg[8])])
+                            val = struct.unpack(">H", byteVal)[0]
+                            sensorData['WindPulses'] = int(val)
+                            # windFactor = 0.004804488 # pulses per km/h
+                            windFactor = 0.004804488 * 3 # pulses per km/h ()
+                            sensorData['WindSpeed'] = round(float(val) * windFactor, 1)
+
+                            # Wind gust past 5 min (8 sec) (unsigned int)
+                            byteVal = bytearray([int(msg[11]), int(msg[10])])
+                            val = struct.unpack(">H", byteVal)[0]
+                            sensorData['WindGustPulses'] = int(val)
+                            # WindGust is in a 8 sec time frame. windSpeed factor is in 300s time frame
+                            # 300s / 8s = 37.5
+                            sensorData['WindGust'] = round(float(val) * windFactor * 37.5, 1)
+                
+                            # Nr of rain pulses in past 5 min (unsigned int)
+                            # 1 liter of water on a m2 is 1 mm of rain
+                            # 1 liter of water in the rain bucket gives 315 pulses
+                            # The rain bucket is 180mm in diameter and that gives an area of 25446.9mm2=0.0254469m2
+                            # This area is 39.2975168 times smaller than a m2
+                            # 315 pulses is 1 liter and 1 mm of rain/m2, 1 pulse is 1/315mm multiplied by the size factor
+                            # So every pulse = 1/315 * 39.2975168 = 0.124754mm/pulse
+                            byteVal = bytearray([int(msg[13]), int(msg[12])])
+                            val = struct.unpack(">H", byteVal)[0]
+                            sensorData['RainPulses'] = int(val)
+                            sensorData['RainFall'] = float(val) * 0.124754
+                            # rainRate is mm/h, measurement frame is 5 min. 60min / 5 = 12
+                            sensorData['RainRate'] = round(float(val) * 0.124754 * 12, 1)
+
+                            mqtt_publish.single("huis/LoRa/Weerstation/weer", json.dumps(sensorData, separators=(', ', ':')), hostname=settings.MQTT_ServerIP, retain=True)
+                        else:
+                            print("LoRa: Received unknown msgId:%d from NodeId %d" % (msgId, nodeId))
+
+                    # nodeId 5: Temp Chata
+                    # nodeId 6: Temp Chata Kelder
+                    # nodeId 7: Temp Hottub
+                    # nodeId 8: Temp Schuur
+                    # nodeId 9: Temp Sauna
+                    # nodeId 10: Temp Vriezer Werkplaats
+                    # nodeId 11: Temp Vriezer Keuken
+                    # nodeId 12: Temp Vriezer Technische ruimte
+                    # nodeId 13..29 Voor toekomstige tempsensoren
+                    elif (nodeId >= 5) and (nodeId <= 29):
+                        msgId = int(msg[0])
+                        del msg[0]  # remove msgId from list
+
+                        # MSG_ID_NODE_STARTUP=1: Node startup notification
+                        if msgId == 1:
+                            print("Received Temperature node (id %d) startup notification" % (nodeId))
                         # MSG_ID_TEMPERATURE=4: Temperature measurement
                         elif msgId == 4:
                             sensorData = {}
@@ -250,7 +371,11 @@ def serialPortThread():
 
                             byteVal = bytearray([int(msg[2]), int(msg[1])])
                             val = struct.unpack(">h", byteVal)[0]
-                            sensorData['Temperature'] = round(float(val) / 10, 1)
+                            if val != DEVICE_DISCONNECTED:
+                                sensorData['Temperature'] = round(float(val) / 10, 1)
+                            else:
+                                # Sensor device disconnected
+                                sensorData['Temperature'] = DEVICE_DISCONNECTED
 
                             if nodeId == 5:
                                 print("Temp Chata %1.1f C" % (val / 10))
@@ -262,12 +387,20 @@ def serialPortThread():
                                 print("Temp Hottub Water Boven %1.1f C" % (val / 10))
                                 byteVal = bytearray([int(msg[4]), int(msg[3])])
                                 val = struct.unpack(">h", byteVal)[0]
-                                sensorData['TempWaterBeneden'] = round(float(val) / 10, 1)
+                                if val != DEVICE_DISCONNECTED:
+                                    sensorData['TempWaterBeneden'] = round(float(val) / 10, 1)
+                                else:
+                                    # Sensor device disconnected
+                                    sensorData['TempWaterBeneden'] = DEVICE_DISCONNECTED
                                 print("Temp Hottub Water Beneden %1.1f C" % (val / 10))
 
                                 byteVal = bytearray([int(msg[6]), int(msg[5])])
                                 val = struct.unpack(">h", byteVal)[0]
-                                sensorData['TempFiltratieKast'] = round(float(val) / 10, 1)
+                                if val != DEVICE_DISCONNECTED:
+                                    sensorData['TempFiltratieKast'] = round(float(val) / 10, 1)
+                                else:
+                                    # Sensor device disconnected
+                                    sensorData['TempFiltratieKast'] = DEVICE_DISCONNECTED
                                 print("Temp Hottub FiltratieKast %1.1f C" % (val / 10))
                                 
                                 mqtt_publish.single("huis/LoRa/Temp-Hottub/temp", json.dumps(sensorData, separators=(', ', ':')), hostname=settings.MQTT_ServerIP, retain=True)
